347afc6b775e7eed99d450b87e2a8ef1e56c00c3
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-06-25 15:06:41 +0300

    Add project files.

---
+++ AjaxProject/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AjaxProject.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=YUCESAFA\\SQLEXPRESS; initial Catalog=AjaxDb; integrated Security=true");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
